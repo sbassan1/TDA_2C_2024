ed2441313682eaf8ee67c18dedb5f182ea1aefe4
sbassan1 <105613188+sbassan1@users.noreply.github.com>
2024-08-24 15:25:21 -0300

    Comienzos de astroTrade

--- Practica 1/dinamicProgramming/7astroTrade.cpp
+++ Practica 1/dinamicProgramming/7astroTrade.cpp
@@ -6,22 +6,20 @@ using namespace std;
 
 int INF = 2e5;
 
+int astroTrade(vector<int>& p, int i, int j){
+
+
+    
+
+
+}
 
 int main() {
-/*     int costo = 14;  // Costo del producto
-    vector<int> billetera{2, 3, 5, 10, 20, 20};  // Cantidad de billetes y su denominacion */
 
-    int costo = 9;  // Costo del producto
-    vector<int> billetera{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};  // Cantidad de billetes y su denominacion
-    int n = billetera.size();
+    vector<int> p{3,2,5,6};
+    int n = p.size();
 
-    mem = vector<vector<pair<int, int>>>(n + 1, vector<pair<int, int>>(costo + 1, {INF, INF}));
-
-    pair<int, int> resultadoBacktracking = optipagoBackTracking(billetera, n, costo);
-    pair<int, int> resultadoTopDown = optipagoTopDown(billetera, n, costo);
-
-    cout << "Menor cantidad de billetes usados (Backtracking): " << resultadoBacktracking.first << endl;
-    cout << "Menor cantidad de billetes usados (TopDown): " << resultadoTopDown.first << endl;
+    astroTrade(p)
 
     return 0;
 }
