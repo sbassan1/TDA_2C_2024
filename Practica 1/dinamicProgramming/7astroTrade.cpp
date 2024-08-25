1192a1231f89f97e3fb3c6008179dedc816fddfc
Santiago Bassani <santiagobassani2000@gmail.com>
2024-08-25 00:29:42 -0300

    astroTrade completado

--- Practica 1/dinamicProgramming/7astroTrade.cpp
+++ Practica 1/dinamicProgramming/7astroTrade.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <tuple>
 
 using namespace std;
 
 int INF = 2e5;
 
-int astroTrade(vector<int>& p, int i, int j){
+// Definir un map para la memorización
+map<tuple<int, int, int>, int> mem;
 
+int astroTrade(vector<int>& p, int i, int c, int j){ // i es el dia de compra, j es la cantidad de asteroides, c es la ganancia
 
+    if (i == p.size()) { // Llegamos al final de la semana
+        if(j == 0) return c; // Como en 0 es la solucion optima la guardo
+        if(j != 0) return -INF;
+    }
+
+    // Crear la clave para la memorización
+    tuple<int, int, int> key = make_tuple(i, c, j);
+
+    // Verificar si ya calculamos esta solución
+    if(mem.find(key) != mem.end()) return mem[key];
+
+    if( (p.size() - i) < j){ // Si la cantidad de dias restantes es menor a la cantidad de asteroides a vender
+        return -INF;
+    }
+
+    int comprarAsteroide = astroTrade(p, i + 1, c - p[i] , j + 1);
+    int noHacerNada = astroTrade(p, i + 1, c , j);
+    int venderAsteroide = astroTrade(p, i + 1, c + p[i] , j - 1);
     
-
-
+    // Guardar la mejor solución en el map
+    return mem[key] = max({comprarAsteroide, noHacerNada, venderAsteroide});
 }
 
 int main() {
 
-    vector<int> p{3,2,5,6};
-    int n = p.size();
-
-    astroTrade(p)
+    vector<int> p{3,6,10};
+    cout << "Maxima ganancia posible: " << astroTrade(p , 0, 0, 0) << endl;
 
     return 0;
 }
+
+
