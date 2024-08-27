ec7d452c1722c59c00cc7924c1f937e38ed9f1a3
Santiago Bassani <santiagobassani2000@gmail.com>
2024-08-27 19:59:00 -0300

    Avances con cortes

--- Practica 1/dinamicProgramming/8cortesEconomicos.cpp
+++ Practica 1/dinamicProgramming/8cortesEconomicos.cpp
@@ -8,10 +8,19 @@ using namespace std;
 
 int INF = 2e5;
 
+int cortesEconomicos(vector<int>& p, int l, int i)
+{
+
+    cortesEconomicos() + 
+}
+
+
 int main() {
 
-    vector<int> p{3,6,10};
-    cout << "Maxima ganancia posible: " << astroTrade(p , 0, 0, 0) << endl;
+    int l = 10;             // longitud 
+    vector<int> p{3,6,10};  // lista de cortes
+
+    cout << "Maxima ganancia posible: " <<  << endl;
 
     return 0;
 }
