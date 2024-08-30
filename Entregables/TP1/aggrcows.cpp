12a1371219429171969117ce357d120a3034eba9
sbassan1 <105613188+sbassan1@users.noreply.github.com>
2024-08-29 21:20:26 -0300

    Llegué a tener un resultado en aggrcows con bktrac

--- Entregables/TP1/aggrcows.cpp
+++ Entregables/TP1/aggrcows.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,30 +13,47 @@ using namespace std;
 4
 9 
  */
-int cantVacas( vector<int> &cubiculos, int i, int &distaciaMinimaMaxima, )
 
+// Necesito mantener calculado la distancia minima entre las vacas entre cubiculos de cada permutacion y devolverla la mayor
+
+int distanciaMinimaMaxima(vector<int>& cubiculos, int vacasRestantes, int i, int ultimaPosicion, int distanciaMinima) {
+    if (vacasRestantes == 0) {
+        return distanciaMinima;  // Todas las vacas han sido colocadas, devolver la distancia mínima calculada
+    }
+
+    if (i == cubiculos.size()) {
+        return -INT_MAX;  // No se pueden colocar todas las vacas
+    }
+
+    int noAgregamosVaca = distanciaMinimaMaxima(cubiculos, vacasRestantes, i + 1, ultimaPosicion, distanciaMinima);
+
+    int nuevaDistanciaMinima = distanciaMinima; // Nosotros comenzamos sin agregar vacas con distancia minima en INT MAX
+
+    if (ultimaPosicion != -1) {  // Si no es la primera vaca
+        nuevaDistanciaMinima = min(distanciaMinima, cubiculos[i] - ultimaPosicion); // Verificamos que la distancia entre la nueva vaca es menor
+    }
+    int agregamosVaca = distanciaMinimaMaxima(cubiculos, vacasRestantes - 1, i + 1, cubiculos[i], nuevaDistanciaMinima); 
+
+    return max(noAgregamosVaca, agregamosVaca);
+}
 
 int main() {
-
-    int t;   // T casos
+    int t;  // T casos
     cin >> t;
 
-    for(int i = 0; i < t; i++){ // Hago loop por los casos
-
-        int n, c; // N cubiculos, C vacas
+    while (t--) {
+        int n, c;  // N cubículos, C vacas
         cin >> n >> c;
 
         vector<int> cubiculos(n);
-
-        for(int j = 0; j < n; j++){ // Cantidad de cubiculos
-            cin >> cubiculos[j];
+        for (int i = 0; i < n; i++) {
+            cin >> cubiculos[i];
         }
 
-        cout << endl << "Vector Actual: ";
-        for(int x = 0; x < n ; x++){
-            cout << cubiculos[x] << " ";
-        }
-        cout << endl;
+        sort(cubiculos.begin(), cubiculos.end()); // Acá sorteo la lista de posiciones para tenerlas de menor a mayor
+
+        int mejorDistanciaMinima = distanciaMinimaMaxima(cubiculos, c, 0, -1, INT_MAX);
+        cout << mejorDistanciaMinima << endl;
     }
 
     return 0;
