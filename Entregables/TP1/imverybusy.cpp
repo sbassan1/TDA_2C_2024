848bc35bea683ae7cc4ff5f53b1153bfede949ad
Santiago Bassani <santiagobassani2000@gmail.com>
2024-08-31 15:52:42 -0300

    imverybusy terminado con top down

---
+++ Entregables/TP1/imverybusy.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <utility> 
+#include <algorithm>
+
+using namespace std;
+
+int maximasActividades(vector<pair<int, int>>& tareas, int i, int ultimoSeleccionado, vector<vector<int>>& memo) {
+    if (i == tareas.size()) return 0; // llegamos al final del vector
+
+    if (memo[i][ultimoSeleccionado + 1] != -1) return memo[i][ultimoSeleccionado + 1];
+
+    int noHagoActividad = maximasActividades(tareas, i + 1, ultimoSeleccionado, memo);
+
+    int hagoActividad = 0;
+    if (ultimoSeleccionado == -1 || !(tareas[ultimoSeleccionado].second > tareas[i].first)) {
+        hagoActividad = 1 + maximasActividades(tareas, i + 1, i, memo);
+    }
+
+    return memo[i][ultimoSeleccionado + 1] = max(hagoActividad, noHagoActividad);
+}
+
+int main() {
+    int t; // Numero de tests
+    cin >> t;
+
+    vector<int> soluciones;
+
+    while (t > 0) {
+        int n; // Numero de pares en este test
+        cin >> n;
+
+        vector<pair<int, int>> tareas(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> tareas[i].first >> tareas[i].second;
+        }
+
+        sort(tareas.begin(), tareas.end(), [](pair<int, int>& a, pair<int, int>& b) {
+            return a.second < b.second;
+        });
+
+        vector<vector<int>> memo(n, vector<int>(n + 1, -1)); // Inicializo la mem con n espacios en -1
+
+        int resultado = maximasActividades(tareas, 0, -1, memo);
+
+        soluciones.push_back(resultado);
+
+        t--;
+    }
+
+    for(int i = 0; i < soluciones.size(); i++){
+        cout << soluciones[i] << endl;
+    }
+
+    return 0;
+}
