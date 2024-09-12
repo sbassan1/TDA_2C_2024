219360c35b6beabb24b89ebac2e39ca50ec514cf
Santiago Bassani <santiagobassani2000@gmail.com>
2024-09-12 19:36:09 -0300

    blckwhite con podas en top-down con map

--- Entregables/TP1/blackorwhite.cpp
+++ Entregables/TP1/blackorwhite.cpp
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <map>
 
-using namespace std;
-
 /*
 Razonamiento: 
 
@@ -31,55 +29,54 @@ Lo que puedo hacer en la recursion entonces es:
 
  */
 
-int INF = 99999999;
+using namespace std;
 
+int INF = 99999999;
 map<tuple<int, int, int>, int> memo;
 
-int blackOrWhite(vector<int>& secuenciaNumeros, int i, int n, int ultimoBlanco, int ultimoNegro){
+int blackOrWhite(vector<int>& secuenciaNumeros, int i, int n, int ultimoBlanco, int ultimoNegro, int maxActual) {
     if (i == n) return 0;
 
     tuple<int, int, int> decisionActual = make_tuple(i, ultimoBlanco, ultimoNegro);
-
     if (memo.find(decisionActual) != memo.end()) return memo[decisionActual];
 
-    int agregoNegro = 0, agregoBlanco = 0, agregoNinguno;
+    int numRestantes = n - i;
+    int maxDecisionActual = maxActual + numRestantes;
+    if (maxDecisionActual <= memo[decisionActual]) {
+        return 0;
+    }
 
+    int agregoNegro = 0, agregoBlanco = 0, agregoNinguno;
+    agregoNinguno = blackOrWhite(secuenciaNumeros, i+1, n, ultimoBlanco, ultimoNegro, maxActual);
 
     if (secuenciaNumeros[i] > ultimoNegro) {
-        agregoNegro = blackOrWhite(secuenciaNumeros, i+1, n, ultimoBlanco, secuenciaNumeros[i]) + 1;
+        agregoNegro = blackOrWhite(secuenciaNumeros, i+1, n, ultimoBlanco, secuenciaNumeros[i], maxActual + 1) + 1;
     }
 
     if (secuenciaNumeros[i] < ultimoBlanco) {
-        agregoBlanco = blackOrWhite(secuenciaNumeros, i+1, n, secuenciaNumeros[i], ultimoNegro) + 1;
+        agregoBlanco = blackOrWhite(secuenciaNumeros, i+1, n, secuenciaNumeros[i], ultimoNegro, maxActual + 1) + 1;
     }
-
-    agregoNinguno = blackOrWhite(secuenciaNumeros, i+1, n, ultimoBlanco, ultimoNegro);
 
     memo[decisionActual] = max({agregoNegro, agregoBlanco, agregoNinguno});
     return memo[decisionActual];
-
-
 }
 
-
-int main(){
-    
+int main() {
     while (true) {
-        int n; // Numero de numeros
+        int n; // Número de números
         cin >> n;
 
         if (n == -1) break;
 
         vector<int> secuenciaNumeros(n);
-
         for (int i = 0; i < n; i++) {
             cin >> secuenciaNumeros[i];
         }
 
         memo.clear();
+        int maxActual = 0;
 
-        cout << n - blackOrWhite(secuenciaNumeros, 0, n, INF, -INF) << endl;
-
+        cout << n - blackOrWhite(secuenciaNumeros, 0, n, INF, -INF, maxActual) << endl;
     }
     return 0;
 }
