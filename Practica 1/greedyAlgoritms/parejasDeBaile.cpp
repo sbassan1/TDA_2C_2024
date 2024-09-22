2d78fa50d7ef66cc4177f2690bbf0c714d587440
sbassan1 <105613188+sbassan1@users.noreply.github.com>
2024-09-22 15:05:42 -0300

    Solucion del problema 1 de greedy de la pract 1

--- Practica 1/greedyAlgoritms/parejasDeBaile.cpp
+++ Practica 1/greedyAlgoritms/parejasDeBaile.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
-#include <algorithm>
+/* 
+Tenemos dos conjuntos de personas y queremos armar la maxima cantidad de parejas de bailarines.
+Los numeros de los dos conjuntos representan la habilidad de cada una de las personas. Para
+armar parejas estas no deben tener mas de 1 numero de habilidad de diferencia.
 
+- Los numeros de habilidades estan en orden ascendiente
+- Tengo que buscar : el numero a comparar (+-1)
+ 
+ */
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    int n = 5; // cantidad de cajas
+    vector<int> conj1({1,1,1,1,1});
+    vector<int> conj2({1,2,3});
     
+    int i = 0, j = 0;
+    int cont = 0;
     
+    while (i < conj1.size() && j < conj2.size()) {
+        if (abs(conj1[i] - conj2[j]) <= 1) {
+            cont++;
+            i++;
+            j++;
+        } else if (conj1[i] < conj2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    cout << cont << endl;
 
     return 0;
 }
