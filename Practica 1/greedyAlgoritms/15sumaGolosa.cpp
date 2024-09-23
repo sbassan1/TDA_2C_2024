d2289fd11db106a716a7e2ca3a5ae15e04eb9c1f
sbassan1 <105613188+sbassan1@users.noreply.github.com>
2024-09-23 00:01:51 -0300

    Carpeta y pdf prac 2 y 15sumaGolosa greedy listo

--- Practica 1/greedyAlgoritms/15sumaGolosa.cpp
+++ Practica 1/greedyAlgoritms/15sumaGolosa.cpp
@@ -5,5 +5,30 @@ using namespace std;
 
 int main() {
 
+    //vector<int> nums = {3, 7, 1, 9, 4, 6};
+    vector<int> nums = {1,2,5};
+
+    sort(nums.begin(), nums.end()); // Segun internet esta version de sort no es estable y su complejidad es O (n log n)
+
+    int n = nums.size();
+
+    int costo = nums[0] + nums[1];
+    int costoTotal = 0;
+
+    for(int i = 2; i < (n) ; i++){
+        costoTotal = costo + (costo + nums[i]);
+        costo += nums[i]; 
+    } 
+
+    cout << costoTotal << endl;
+
     return 0;
 }
+
+
+/* 
+Si ordenamos el conjunto de menor a mayor obtenemos el conjunto con el costo mas pequeño
+ya que al sumar costos desde los numeros mas chicos vamos acumulando un costo total menor
+comparado a sumar con numeros mas grandes. Esto se debe a que estamos acumulando el costo
+basandose en la priera suma, y eligiendo elementos de menor valor minimiza esta.
+ */
