dabddd8d5a80e0599567dc76451f3f443d011d84
Santiago Bassani <santiagobassani2000@gmail.com>
2024-09-30 09:27:38 -0300

    practica 3 agregada

---
+++ Practica 2/5potenciaSum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* 
+Tengo una matriz 4x4
+
+|11,12,13,14|
+|21,22,23,24|
+|31,32,33,34|
+|41,42,43,44|
+
+Tengo que computar
+
+y devolver A^1 + A^2 + A^3 + ... + A^n
+
+con divide and conquer. En una complejidad menor a O(n)
+ */
+
+
+
+
+int main() {
+
+    cout << pot(2,9);
+
+    return 0;
+}
